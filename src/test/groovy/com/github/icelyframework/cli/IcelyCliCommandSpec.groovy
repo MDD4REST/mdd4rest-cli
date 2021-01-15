35972c82db7810a11601591a142d9be862163b89
amirdeljouyi <amirdeljouyi@gmail.com>
2021-01-15 15:52:12 +0330

    Project Configuration: Micronaut + Gradle + GraalVM + Picocli was added

---
+++ src/test/groovy/com/github/icelyframework/cli/IcelyCliCommandSpec.groovy
@@ -0,0 +1,30 @@
+package com.github.icelyframework.cli
+
+import io.micronaut.configuration.picocli.PicocliRunner
+import io.micronaut.context.ApplicationContext
+import io.micronaut.context.env.Environment
+
+import spock.lang.AutoCleanup
+import spock.lang.Shared
+import spock.lang.Specification
+
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+
+class IcelyCliCommandSpec extends Specification {
+
+    @Shared @AutoCleanup ApplicationContext ctx = ApplicationContext.run(Environment.CLI, Environment.TEST)
+
+    void "test icely-cli with command line option"() {
+        given:
+        ByteArrayOutputStream baos = new ByteArrayOutputStream()
+        System.setOut(new PrintStream(baos))
+
+        String[] args = ['-v'] as String[]
+        PicocliRunner.run(IcelyCliCommand, ctx, args)
+
+        expect:
+        baos.toString().contains('Hi!')
+    }
+}
+
